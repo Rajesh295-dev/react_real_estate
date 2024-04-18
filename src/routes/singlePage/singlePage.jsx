2fe6c67094b54b6bffb4d75dcab1a82cbe3a6d51
Rajesh295-dev <rajeshgautam766@yahoo.com>
2024-04-18 00:57:14 -0400

    added more pages🖼️

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,13 +1,35 @@
 import "./singlePage.scss";
-
-import { listData } from "../../component/library/dummyData";
+import Slider from "../../component/slider/slider";
+import { singlePostData, userData } from "../../component/library/dummyData";
 
 function SinglePage() {
-  const data = listData;
   return (
     <div className="singlePage">
-      <div className="listContainer">lsit</div>
-      <div className="mapContainer">map</div>
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png"></img>
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img}></img>
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">{singlePostData.description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
     </div>
   );
 }
