32e55e6a11ee08bf6752c5fbae00a2195d6f8ed3
Rajesh295-dev <rajeshgautam766@yahoo.com>
2025-01-01 23:42:17 -0500

    added media

--- client/src/routes/listPage/listPage.jsx
+++ client/src/routes/listPage/listPage.jsx
@@ -60,6 +60,60 @@
 
 // export default ListPage;
 
+// import "./listPage.scss";
+// import Filter from "../../component/filter/filter";
+// import Card from "../../component/card/card";
+// import Map from "../../component/map/map";
+// import { Await, useLoaderData } from "react-router-dom";
+// import { Suspense } from "react";
+
+// function ListPage() {
+//   const data = useLoaderData();
+//   return (
+//     <div className="listPage">
+//       <div className="listContainer">
+//         <div className="wrapper">
+//           <Filter />
+
+//           <Suspense fallback={<p>Loading posts...</p>}>
+//             <Await
+//               resolve={data.postResponse}
+//               errorElement={<p>Error loading posts!</p>}
+//             >
+//               {(postResponse) =>
+//                 postResponse.data.map((post) => (
+//                   <Card
+//                     key={post.id}
+//                     item={{
+//                       id: post.id,
+//                       title: post.title,
+//                       price: post.price,
+//                       images: post.images,
+//                       address: post.address,
+//                     }}
+//                   />
+//                 ))
+//               }
+//             </Await>
+//           </Suspense>
+//         </div>
+//       </div>
+//       <div className="mapContainer">
+//         <Suspense fallback={<p>Loading map...</p>}>
+//           <Await
+//             resolve={data.postResponse}
+//             errorElement={<p>Error loading map data!</p>}
+//           >
+//             {(postResponse) => <Map items={postResponse.data} />}
+//           </Await>
+//         </Suspense>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ListPage;
+
 import "./listPage.scss";
 import Filter from "../../component/filter/filter";
 import Card from "../../component/card/card";
@@ -69,6 +123,7 @@ import { Suspense } from "react";
 
 function ListPage() {
   const data = useLoaderData();
+
   return (
     <div className="listPage">
       <div className="listContainer">
@@ -77,22 +132,26 @@ function ListPage() {
 
           <Suspense fallback={<p>Loading posts...</p>}>
             <Await
-              resolve={data.postResponse}
+              resolve={data?.postResponse}
               errorElement={<p>Error loading posts!</p>}
             >
               {(postResponse) =>
-                postResponse.data.map((post) => (
-                  <Card
-                    key={post.id}
-                    item={{
-                      id: post.id,
-                      title: post.title,
-                      price: post.price,
-                      images: post.images,
-                      address: post.address,
-                    }}
-                  />
-                ))
+                Array.isArray(postResponse?.data) ? (
+                  postResponse.data.map((post) => (
+                    <Card
+                      key={post.id}
+                      item={{
+                        id: post.id,
+                        title: post.title,
+                        price: post.price,
+                        images: post.images,
+                        address: post.address,
+                      }}
+                    />
+                  ))
+                ) : (
+                  <p>No posts available</p>
+                )
               }
             </Await>
           </Suspense>
@@ -101,10 +160,16 @@ function ListPage() {
       <div className="mapContainer">
         <Suspense fallback={<p>Loading map...</p>}>
           <Await
-            resolve={data.postResponse}
+            resolve={data?.postResponse}
             errorElement={<p>Error loading map data!</p>}
           >
-            {(postResponse) => <Map items={postResponse.data} />}
+            {(postResponse) =>
+              Array.isArray(postResponse?.data) ? (
+                <Map items={postResponse.data} />
+              ) : (
+                <p>No map data available</p>
+              )
+            }
           </Await>
         </Suspense>
       </div>
