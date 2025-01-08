8daf6b61567f84753bf54483a9755901197eef1e
Rajesh295-dev <rajeshgautam766@yahoo.com>
2025-01-08 01:11:30 -0500

    added list/map toggle

--- client/src/library/apiRequest.js
+++ client/src/library/apiRequest.js
@@ -1,10 +1,34 @@
 
+// import axios from "axios";
+// console.log("Base URL:", import.meta.env.VITE_REACT_APP_BASE_URL);
+
+// const apiRequest = axios.create({
+//     baseURL: import.meta.env.VITE_REACT_APP_BASE_URL, // Access baseURL from .env
+//     withCredentials: true,
+// });
+
+// export default apiRequest;
+
+
+
 import axios from "axios";
+
 console.log("Base URL:", import.meta.env.VITE_REACT_APP_BASE_URL);
 
 const apiRequest = axios.create({
-    baseURL: import.meta.env.VITE_REACT_APP_BASE_URL, // Access baseURL from .env
-    withCredentials: true,
+    baseURL: import.meta.env.VITE_REACT_APP_BASE_URL,
+    withCredentials: true, // Include cookies in requests
+});
+
+// Add Authorization token to every request
+apiRequest.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
 });
 
 export default apiRequest;
