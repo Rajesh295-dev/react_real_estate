055f2e3b93b0eceea4c1df59fe74aeb62c01e25c
Rajesh295-dev <rajeshgautam766@yahoo.com>
2025-01-08 18:13:14 -0500

    added list/map toggle

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -2,10 +2,10 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
     // Ensure the token variable is properly initialized
-    //const token = req.cookies?.token; // Safely access cookies object
-    const token =
-        req.cookies?.token ||
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MzE5OGUwMGQ0NTA3OTgwMDMwYmU1YyIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE3MzYzNzY5MjIsImV4cCI6MjM0MTE3NjkyMn0.0M-SpNnO8Qxb3bJXacKhJkVtypOTf4TdCRPn7oQM_sI";
+    const token = req.cookies?.token; // Safely access cookies object
+    // const token =
+    //     req.cookies?.token ||
+    //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MzE5OGUwMGQ0NTA3OTgwMDMwYmU1YyIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE3MzYzNzY5MjIsImV4cCI6MjM0MTE3NjkyMn0.0M-SpNnO8Qxb3bJXacKhJkVtypOTf4TdCRPn7oQM_sI";
 
     if (!token) {
         return res.status(401).json({ message: "Token is missing" });
